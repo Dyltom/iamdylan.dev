463388d26fa823aa4e1bee0b00071e79bd3b15d0
dyltom <dylanthenderson@yahoo.com.au>
2023-11-13 15:27:43 +1100

    small wording tweak

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -24,7 +24,7 @@ const HeroSection: React.FC = () => {
       <Typography variant="subtitle1" align="left">
         I'm a Fullstack Developer with expertise in React, Typescript, and
         building restful APIs using Node & Koa. My coding journey started with
-        crafting my first Minecraft plugin in 2015, sparking my passion for
+        crafting my first Minecraft plugin in 2014, sparking my passion for
         continuous learning.
       </Typography>
       <Typography align="left">
